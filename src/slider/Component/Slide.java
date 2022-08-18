b1c17b82beb3ccbeba623ca472bca238948d123f
user <user@DESKTOP-LKTQ0L2>
2022-08-18 12:48:49 +0100

    Added Files

--- src/slider/Component/Slide.java
+++ src/slider/Component/Slide.java
@@ -8,31 +8,77 @@ import org.jdesktop.animation.timing.Animator;
 import org.jdesktop.animation.timing.TimingTarget;
 import org.jdesktop.animation.timing.TimingTargetAdapter;
 
-
+/**
+ *
+ * @author user
+ */
 public class Slide extends javax.swing.JLayeredPane {
 
- private final JPanel panel;
- private final MigLayout layout;
- private final Animator animate;
- private Component componentShow;
- private Component componentOut;
- private int SelectedIndex;
- private boolean selected;
- 
+  private final Animator animate;
+  private final MigLayout layout;
+  private final JPanel panel;
+  private Component componentShow;
+  private Component ComponentOut;
+  private int SelectedIndex;
+  private boolean selected;
+  
     public Slide() {
         initComponents();
         layout = new MigLayout("inset 0");
         panel = new JPanel();
-        TimingTarget target = new TimingTargetAdapter(
-        );
+        TimingTarget target = new TimingTargetAdapter();
         
         animate = new Animator(1000, target);
         animate.setResolution(0);
         animate.setAcceleration(0.5f);
         animate.setDeceleration(0.5f);
+        panel.setLayout(layout);
+        setLayout(new MigLayout("fill, inset 0", "[fill, center]", "3[fill]3"));
+        add(panel, "w 100%");
     }
 
-   
+    public void getSlider(Component ...com){
+        if (com.length >= 2) {
+            for(Component coms : com){
+                coms.setVisible(false);
+                panel.add(coms, "pos 0 0 0 0");
+            }
+        }
+        if(panel.getComponentCount()>0){
+            componentShow = panel.getComponent(0);
+            componentShow.setVisible(true);
+            layout.setComponentConstraints(componentShow, "pos 0 0 100% 100%");
+        }
+    }
+
+    private void Next(){
+        if (animate.isRunning()) {
+            selected = true;
+            SelectedIndex = setNext(SelectedIndex);
+            componentShow = panel.getComponent(SelectedIndex);
+            ComponentOut = panel.getComponent(SelectedIndex -1);
+            animate.start();
+            
+        }
+        
+    }
+    private int setNext(int index){
+        if (index == panel.getComponentCount() -1) {
+            return 0;
+        }else{
+            return index;
+        }
+        
+    }
+    
+    private int checkNext(int index){
+        if (index == -1) {
+            return panel.getComponentCount() -1;
+            
+        }else{
+            return index;
+        }
+    }
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {

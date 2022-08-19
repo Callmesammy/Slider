0c9dd30b364a1262c419f53a424ebb48f2d2b9c9
user <user@DESKTOP-LKTQ0L2>
2022-08-19 19:11:46 +0100

    Added Files

--- src/slider/Main.java
+++ src/slider/Main.java
@@ -13,7 +13,7 @@ public class Main extends javax.swing.JFrame {
         SlideCom.getSlider(new Slider1(), new Slider2());
     }
 
-
+hg
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
